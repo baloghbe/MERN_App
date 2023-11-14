7f6d164e0c1209649e222340d957d8a2648d4008
baloghbe <92228708+baloghbe@users.noreply.github.com>
2023-11-14 17:15:40 +0100

    Mobile responsiveness added

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -45,11 +45,11 @@ const Form = ({currentId, setCurrentId}) =>{
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant ="h6">${currentId ? 'Editing' : 'Creating'} a Memory</Typography>
+            <Typography variant ="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
             <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}></TextField>
             <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}></TextField>
             <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}></TextField>
-            <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}></TextField>
+            <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}></TextField>
             <div className={classes.fileInput}>
                 <FileBase 
                     type="file"
