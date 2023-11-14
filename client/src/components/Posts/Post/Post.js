b715e8f4d0d40760950b1e6ba1b81fd3e08b53d7
baloghbe <92228708+baloghbe@users.noreply.github.com>
2023-11-14 14:49:50 +0100

    Delete functionality added

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,9 +6,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from "react-redux";
+
+import { deletePost } from "../../../actions/posts";
 
 const Post = ({post, setCurrentId}) =>{
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return (
         <Card className={classes.card}>
@@ -40,7 +44,7 @@ const Post = ({post, setCurrentId}) =>{
                 </Button>
             </CardActions>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() =>{}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
